7f76dc26ad597b19da5543f4aacbb4e73cb528bf
Danat Yermakovich <danatyermakovich@gmail.com>
2019-10-30 13:15:53 +0300

    i hope it works

---
+++ danat_work/com_plink.sh
@@ -0,0 +1,1 @@
+plink --vcf ancestry-reference-2019-10-28.pruned.vcf.gz --pca --out f_try
---
+++ danat_work/git.sh
@@ -0,0 +1,6 @@
+echo "# work" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/ngs19popgen/work.git
+git push -u origin master
